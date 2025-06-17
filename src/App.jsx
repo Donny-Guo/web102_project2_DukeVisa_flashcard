8c6089ff6f76b7587cd84f6accbb51d9b33d6f1c
Donny <107092000+Donny-Guo@users.noreply.github.com>
2025-06-17 11:17:38 -0700

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <main>
+      <h1>The Ultimate Plant Parent!</h1>
+
+      <h3>
+        How good of a plant parent are you? Test all of your planty knowledge here!
+      </h3>
+
+      <p>
+        Number of cards: 10
+      </p>
+
+      <button>
+        Prev
+      </button>
+
+      <button>
+        Next
+      </button>
+    </main>
+  )
+}
+
+export default App
